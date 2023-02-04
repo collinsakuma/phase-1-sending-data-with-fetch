456006ab9d66df5087d90fdca0fecfe51bf060ab
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-04 11:29:05 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const bodyTag = document.querySelector('body')
+    const formatData = {
+        name: userName,
+        email: userEmail
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formatData),
+    })
+    .then(res => res.json())
+    .then(data => bodyTag.textContent = data.id)
+    .catch(error => bodyTag.textContent = error.message)
+}
